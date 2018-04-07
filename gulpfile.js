c3765bd1e56e2b9d7e3564d12bfd60af2e982aa0
semioys <semioys@gmail.com>
2018-04-07 21:12:13 +0300

    Gulp sass task was added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp'),
-    pug = require('gulp-pug');
+    plugins = require('gulp-load-plugins')();
 
 gulp.task('pug', function() {
-  return gulp.src('src/pug/pages/*.pug')
-      .pipe(pug({
+  return gulp.src('./src/pug/pages/*.pug')
+      .pipe(plugins.pug({
         pretty: true
       }))
-      .pipe(gulp.dest('build'));
+      .pipe(gulp.dest('./build'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./src/static/sass/main.scss')
+      .pipe(plugins.sass().on('error', plugins.sass.logError))
+      .pipe(plugins.csso())
+      .pipe(plugins.rename('main.min.css'))
+      .pipe(gulp.dest('./build/css'));
 });
