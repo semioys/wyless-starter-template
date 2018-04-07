24adb3a5e3172e997c3b1984477f5b589d97f379
semioys <semioys@gmail.com>
2018-04-07 21:35:26 +0300

    Gulp sourcemaps, autoprefixer, notify plugins were added

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,25 @@ var gulp = require('gulp'),
 
 gulp.task('pug', function() {
   return gulp.src('./src/pug/pages/*.pug')
-      .pipe(plugins.pug({
-        pretty: true
-      }))
-      .pipe(gulp.dest('./build'));
+    .pipe(plugins.pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('sass', function() {
   return gulp.src('./src/static/sass/main.scss')
-      .pipe(plugins.sass().on('error', plugins.sass.logError))
-      .pipe(plugins.csso())
-      .pipe(plugins.rename('main.min.css'))
-      .pipe(gulp.dest('./build/css'));
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass())
+    .on("error", plugins.notify.onError({
+      message: "Error: <%= error.message %>",
+      title: "Error with compilling sass"
+    }))
+    .pipe(plugins.autoprefixer({
+      browsers: ['last 4 versions']
+    }))
+    .pipe(plugins.csso())
+    .pipe(plugins.rename('main.min.css'))
+    .pipe(plugins.sourcemaps.write())
+    .pipe(gulp.dest('./build/css'));
 });
